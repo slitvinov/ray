081b3de27e0dd201cf2022c401c4a9145fe5e844
Sergey Litvinov <slitvinov@gmail.com>
2026-05-11 09:43:01 +0200

    minor

--- aws.sh
+++ aws.sh
@@ -6,8 +6,7 @@ ID=$(aws ec2 run-instances \
     --key-name aws \
     --instance-type t2.nano \
     --image-id ami-0f3caa1cf4417e51b \
-    --network-interfaces '{"AssociatePublicIpAddress":true,"DeviceIndex":0,"Groups":["sg-004d116ede0ce73bb"]}' \
-    --private-dns-name-options '{"HostnameType":"ip-name","EnableResourceNameDnsARecord":true,"EnableResourceNameDnsAAAARecord":false}' \
+    --network-interfaces AssociatePublicIpAddress=true,DeviceIndex=0,Groups=sg-004d116ede0ce73bb \
     --query "Instances[0].InstanceId" --output text)
 echo "launched $ID" >&2
 aws ec2 wait instance-running --instance-ids "$ID"
